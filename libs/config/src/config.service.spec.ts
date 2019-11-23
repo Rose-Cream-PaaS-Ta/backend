0e88c421f10632cfb2c0b59d6100f462d3949c1c
Joosung Park <admin@slowmotion.dev>
2019-11-23 23:36:19 +0900

    add Config Module Test Code

--- libs/config/src/config.service.spec.ts
+++ libs/config/src/config.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { config } from 'dotenv';
+import { resolve } from 'path';
 import { ConfigService } from './config.service';
 
 describe('ConfigService', () => {
   let service: ConfigService;
 
   beforeEach(async () => {
+    config();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConfigService],
+      providers: [{
+        provide: ConfigService,
+        useValue: new ConfigService(resolve(process.cwd(), '.env')),
+      }],
     }).compile();
 
     service = module.get<ConfigService>(ConfigService);
@@ -14,5 +20,11 @@ describe('ConfigService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be get config', () => {
+    expect(service.HOST).toStrictEqual(process.env.HOST || '0.0.0.0');
+    expect(service.NODE_ENV).toStrictEqual(process.env.NODE_ENV || 'development');
+    expect(service.PORT).toStrictEqual(parseInt(process.env.PORT, 10) || 3000);
   });
 });
