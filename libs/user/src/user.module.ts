a6b332906e526b300916678b82c19dffde0cf8c1
Joosung Park <admin@slowmotion.dev>
2019-11-27 13:42:43 +0900

    Add organization module

--- libs/user/src/user.module.ts
+++ libs/user/src/user.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AwardeeModule } from './awardee/awardee.module';
-import { AwarderModule } from './awarder/awarder.module';
+import { OrganizationModule } from './organization/organization.module';
 import { UserService } from './user.service';
 import { ViewerModule } from './viewer/viewer.module';
 
 @Module({
   exports: [UserService],
-  imports: [AwarderModule, AwardeeModule, ViewerModule],
+  imports: [AwardeeModule, ViewerModule, OrganizationModule],
   providers: [UserService],
 })
 export class UserModule {
--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ import { AppModule } from './app.module';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
-    .setTitle('PaaS-Ta')
-    .setDescription('PaaS-Ta API')
-    .setVersion(process.env.npm_package_version)
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  if (config.NODE_ENV === 'development') {
+    const options = new DocumentBuilder()
+      .setTitle('PaaS-Ta')
+      .setDescription('PaaS-Ta API')
+      .setVersion(process.env.npm_package_version)
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(config.PORT, config.HOST);
 };
